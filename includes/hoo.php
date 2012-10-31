mariushoch/Toolserver-hoo

commit d1e9c159db940763c838c0ccad22c9868760f72f

Various database handling enhancements and minor stuff

includes/hoo.php
@@ -17,7 +17,6 @@ class hoo_base {
 	// This function will return a PDO object from which the given $db_name can be accessed
 	//
 	public function &wiki_db($db_name, $user_db = null) {
-		$failure = false;
 		if($db_name === 'toolserver' || $db_name === 'u_hoo_p') {
 			if(!is_array($this->db_connections) || !$this->db_connections['toolserver']) {
 				$this->database_connect($_SQL['misc_db']['server'], 'toolserver', true);
@@ -27,7 +26,7 @@ class hoo_base {
 		$this->load_db_map();
 		if(!isset($this->db_map[ $db_name ])) {
 			//unknown DB
-			return $failure;
+			throw new database_exception('Unknown database: ' . $db_name);
 		}
 		$user_db_server = 'sql-s' . $this->db_map[ $db_name ] . '-user';
 		$rr_db_server = 'sql-s' . $this->db_map[ $db_name ] . '-rr';
@@ -43,20 +42,20 @@ class hoo_base {
 			if($this->database_connect($user_db_server . '.toolserver.org', $user_db_server) === true) {
 				return $this->db_connections[ $user_db_server ];
 			}else{
-				return $failure;
+				throw new database_exception('Couldn\'t connect to user-database: ' . $db_name);
 			}
 		}else{
 			if($this->database_connect($rr_db_server . '.toolserver.org', $rr_db_server) === true) {
 				return $this->db_connections[ $rr_db_server ];
 			}else{
-				return $failure;
+				throw new database_exception('Couldn\'t connect to database: ' . $db_name);
 			}
 		}
 	}
 	//
 	// this function will start a new MySQL PDO connection and store it into $this->db_connections
 	//
-	protected function database_connect($server, $canonical_name, $die_on_error = false, $db = false) {
+	protected function database_connect($server, $canonical_name, $throw_on_error = false, $db = false) {
 		if(!isset($this->db_connections[$canonical_name])) {
 			if(!$this->db_pass) {
 				$ts_pw = posix_getpwuid(posix_getuid());
@@ -72,10 +71,10 @@ class hoo_base {
 			try {
 				$this->db_connections[$canonical_name] = new PDO($tmp, $this->db_user, $this->db_pass);
 			}catch(PDOException $e){
-				if($die_on_error) {
-					$this->show_error($e->getMessage(), 'Connection failed: ');
+				if($throw_on_error) {
+					throw new database_exception('Connection failed: ' . $server);
 				}else{
-					return 'Connection failed: ' . $e->getMessage();
+					return false;
 				}
 			}
 		}
@@ -186,14 +185,20 @@ class hoo_base {
 		global $_SQL;
 		$year = date('o');
 		$week = date('W');
-		$this->database_connect($_SQL['misc_db']['server'], 'toolserver', true);
-		$SQL_query = 'INSERT INTO ' . $_SQL['misc_db']['db_name'] . '.views (page, lang, year, week, views) VALUES (:page, :lang, :year, :week, 1) ON DUPLICATE KEY UPDATE views = views + 1';
-		$statement = $this->db_connections['toolserver']->prepare($SQL_query);
-		$statement->bindValue(':page', $page, PDO::PARAM_STR);
-		$statement->bindValue(':lang', $lang, PDO::PARAM_STR);
-		$statement->bindValue(':year', $year, PDO::PARAM_INT);
-		$statement->bindValue(':week', $week, PDO::PARAM_INT);
-		$statement->execute();
+		try {
+			$this->database_connect($_SQL['misc_db']['server'], 'toolserver', true);
+			$SQL_query = 'INSERT INTO ' . $_SQL['misc_db']['db_name'] . '.views (page, lang, year, week, views) VALUES (:page, :lang, :year, :week, 1) ON DUPLICATE KEY UPDATE views = views + 1';
+			$statement = $this->db_connections['toolserver']->prepare($SQL_query);
+			$statement->bindValue(':page', $page, PDO::PARAM_STR);
+			$statement->bindValue(':lang', $lang, PDO::PARAM_STR);
+			$statement->bindValue(':year', $year, PDO::PARAM_INT);
+			$statement->bindValue(':week', $week, PDO::PARAM_INT);
+			$statement->execute();
+		}catch(Exception $e){
+			// This isn't good, but maybe we still can serve the request
+			log::write_line($e->getMessage(), $e->getFile());
+			return false;
+		}
 		if($statement->rowCount()) {
 			return true;
 		}else{
@@ -237,6 +242,22 @@ class hoo_base {
 			break;
 		}
 	}
+	//
+	// Accept wiki input in different forms (returns the dbname or false):
+	// 1. Database names with trailing _p (like enwiki_p)
+	// 2. Database names without trailing _p (like enwiki)
+	// @TODO: Implement more options
+	//
+	public function wiki_input($input) {
+		$this->load_db_map();
+		// 1. case:
+		if(isset($this->db_map[ $db_name ])) {
+			return $db_name;
+		}
+		// 2. case:
+		if(isset($this->db_map[ $db_name . '_p' ])) {
+			return $db_name . '_p';
+		}
+		return false;
+	}
 }
-
-$hoo = new hoo_base();

includes/tests/hoo.php
@@ -7,6 +7,8 @@ if(!defined('IN_HOO_TS')) {
 	exit();
 }
 
+$hoo = new hoo_base();
+
 // $hoo->get_user_input();
 // Set some $_GET and $_POST values
 
@@ -38,6 +40,15 @@ m_equal('hoo_base:: is_ip("SA01:E35:8A7C:E14F:FGGA:E5FF:FE54:WF1E")', $hoo->is_i
 m_equal('hoo_base:: view_count("unit_test", "unit_test")', $hoo->view_count("unit_test", "unit_test"), true);
 //(this needs post clean up), see section clean up
 
+//test whether $hoo->view_count() really worked
+$db = &$hoo->wiki_db("u_hoo_p");
+m_n_equal('hoo_base:: wiki_db("u_hoo_p")', $db, false);
+$SQL_query = "SELECT views FROM u_hoo_p.views WHERE page = 'unit_test' AND lang = 'unit_test' LIMIT 1";	
+$statement = $db->prepare($SQL_query);
+$statement->execute();
+$tmp = $statement->fetch(PDO::FETCH_ASSOC);
+m_equal('hoo_base:: SQL check for view_count("unit_test", "unit_test")', $tmp['views'], '1');
+
 // $hoo->wiki_db();
 
 //query enwiki_p for my user name
@@ -48,14 +59,13 @@ $statement = $db->prepare($SQL_query);
 $statement->execute();
 $tmp = $statement->fetch(PDO::FETCH_ASSOC);
 m_equal("hoo_base:: Test query on enwiki_p", $tmp['user_name'], 'Hoo man');
-//test whether $hoo->view_count() really worked
-$db = &$hoo->wiki_db("u_hoo_p");
-m_n_equal('hoo_base:: wiki_db("u_hoo_p")', $db, false);
-$SQL_query = "SELECT views FROM u_hoo_p.views WHERE page = 'unit_test' AND lang = 'unit_test' LIMIT 1";	
-$statement = $db->prepare($SQL_query);
-$statement->execute();
-$tmp = $statement->fetch(PDO::FETCH_ASSOC);
-m_equal('hoo_base:: SQL check for view_count("unit_test", "unit_test")', $tmp['views'], '1');
+//invalid wiki
+try{
+	$hoo->wiki_db("not_existing_wiki");
+	went_wrong('hoo_base:: wiki_db("not_existing_wiki");');
+}catch(Exception $e){
+	went_right('hoo_base:: wiki_db("not_existing_wiki");');
+}
 
 //	$hoo->replag();
 
@@ -100,6 +110,22 @@ try{
 	went_right('hoo_base:: replag("xmfwiki_p")');
 }catch(Exception $e){
 	went_wrong('hoo_base:: replag("xmfwiki_p")');
+}
+
+//	$hoo->wiki_input();
+
+// enwiki_p
+m_equal('hoo_base:: wiki_input("enwiki_p")', $hoo->wiki_input("enwiki_p"), 'enwiki_p');
+
+// enwiki
+m_equal('hoo_base:: wiki_input("enwiki")', $hoo->wiki_input("enwiki"), 'enwiki_p');
+
+// not_existing_wiki
+try{
+	$hoo->wiki_input("not_existing_wiki");
+	went_wrong('hoo_base:: wiki_input("not_existing_wiki")');
+}catch(Exception $e){
+	went_right('hoo_base:: wiki_input("not_existing_wiki")');
 }
 
 //

api.php
@@ -14,6 +14,8 @@ error_reporting(-1);
 
 require_once('includes/web_start.php');
 
+$hoo = new hoo_base();
+
 // Output format
 $format = $hoo->get_user_input('format');
 if($format !== null && !in_array($format, machine_readable::$supported_formats)) {

includes/api/ModuleActiveSysops.php
@@ -57,9 +57,6 @@ class apiActiveSysops extends hoo_api {
 		$date = new DateTime('@' . $input['last_action_time']);
 		$last_action_time = $date->format('YmdHis');
 		$db = &$this->wiki_db($input['wiki_db']);
-		if(!$db) {
-			throw new database_exception('Couldn\'t connect to database: ' . $input['wiki_db']);
-		}
 		//get number of active sysops
 		//$input['wiki_db'] is trusty, as the above would have exited, if it wasn't a known DB
 		$SQL_query = 'SELECT COUNT(*) /* LIMIT:15 NM */ AS active_sysops FROM (SELECT log_user AS user FROM ' . $input['wiki_db'] . '.logging WHERE log_type IN ("block","delete","protect") AND log_timestamp > :last_action_time GROUP BY log_user) as active_users INNER JOIN ' . $input['wiki_db'] . '.user_groups ON ug_user = active_users.user WHERE ug_group = "sysop"';

includes/api/ModuleHitCount.php
@@ -59,7 +59,7 @@ class apiHitCount extends hoo_api {
 			throw new Exception('Unknown project or language');
 		}
 		if($this->view_count('hitCount-' . $input['project'], $input['lang'])) {
-			return $this->return_data( array());
+			return $this->return_data(array());
 		}else{
 			throw new database_exception('Database error: ' . $_SQL['misc_db']['db_name']);
 		}

includes/api/ModulePagesCreated.php
@@ -110,9 +110,6 @@ class apiPagesCreated extends hoo_api {
 			}
 		}
 		$db = &$this->wiki_db($input['wiki_db']);
-		if(!$db) {
-			throw new database_exception('Couldn\'t connect to database: ' . $input['wiki_db']);
-		}
 		$SQL_query = 'SELECT /* LIMIT:60 NM*/ sub.* FROM (';
 		$SQL_query .= 'SELECT IF((page_namespace = 0), page_title, CONCAT(namespacename.ns_name , ":", page_title)) AS full_title, page_id, page_namespace, page_is_redirect, rev_timestamp AS creation_time FROM ' . $input['wiki_db'] . '.revision INNER JOIN ' . $input['wiki_db'] . '.page ON page_id = rev_page INNER JOIN toolserver.namespacename ON namespacename.ns_id = page_namespace';
 		$SQL_query .= ' WHERE rev_user_text = :user_name AND rev_parent_id = 0 AND namespacename.dbname = "' . $input['wiki_db'] . '" AND namespacename.ns_is_favorite = 1';

includes/api/ModuleWikiSets.php
@@ -80,9 +80,6 @@ class apiWikiSets extends hoo_api {
 			}
 		}
 		$db = &$this->wiki_db('metawiki_p');
-		if(!$db) {
-			throw new database_exception('Couldn\'t connect to database: ' . $input['wiki_db']);
-		}
 		//get data
 		$SQL_query = 'SELECT ws_id, ' . $return_list . ' FROM centralauth_p.wikiset WHERE ws_id IN(' . $wikiset_list . ')';
 		$statement = $db->prepare($SQL_query);

includes/api_base.php
@@ -58,7 +58,10 @@ abstract class hoo_api extends hoo_base {
 			}else if( $info['type'] === 'wiki' ) {
 				// TODO: Handle different inputs like foo.wikipedia.org,
 				// foowiki_p... probably in hoo_base
-				$tmp = $input[ $info['name'] ];
+				$tmp = $this->wiki_input( $input[ $info['name'] ] );
+				if(!$tmp) {
+					throw new exception('Missing unknown/ invalid wiki: ' . $input[ $info['name'] ]);
+				}
 			}else{
 				$tmp = $input[ $info['name'] ];
 			}

commit 30ad58b7d34c4c08c0c3630a458e61d9a56997f6

Fixing errors introduced with d1e9c159db940763c838c0ccad22c9868760f72f

includes/hoo.php
@@ -251,12 +251,12 @@ class hoo_base {
 	public function wiki_input($input) {
 		$this->load_db_map();
 		// 1. case:
-		if(isset($this->db_map[ $db_name ])) {
-			return $db_name;
+		if(isset($this->db_map[ $input ])) {
+			return $input;
 		}
 		// 2. case:
-		if(isset($this->db_map[ $db_name . '_p' ])) {
-			return $db_name . '_p';
+		if(isset($this->db_map[ $input . '_p' ])) {
+			return $input . '_p';
 		}
 		return false;
 	}

includes/tests/hoo.php
@@ -37,9 +37,8 @@ m_equal('hoo_base:: is_ip("SA01:E35:8A7C:E14F:FGGA:E5FF:FE54:WF1E")', $hoo->is_i
 
 // $hoo->view_count();
 
-m_equal('hoo_base:: view_count("unit_test", "unit_test")', $hoo->view_count("unit_test", "unit_test"), true);
 //(this needs post clean up), see section clean up
-
+m_equal('hoo_base:: view_count("unit_test", "unit_test")', $hoo->view_count("unit_test", "unit_test"), true);
 //test whether $hoo->view_count() really worked
 $db = &$hoo->wiki_db("u_hoo_p");
 m_n_equal('hoo_base:: wiki_db("u_hoo_p")', $db, false);